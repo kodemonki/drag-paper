3f1dde4be2b17298879612f15003997b440fc30d
Tom Jane <kodemonki@hotmail.com>
2019-04-26 17:08:33 +0100

    daily backup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let root = document;
-let ball;
-let radius = 200;
-let interval = null;
-let angle = 45;
+let root = document,
+  ball,
+  radius = 200,
+  interval = null,
+  angle = 45;
 
 draggables = root.querySelectorAll(".draggable");
 
@@ -10,88 +10,68 @@ draggables.forEach(initDrag);
 
 function initDrag(item) {
   ball = item;
-  let rect = ball.getBoundingClientRect();
-  let centerX = document.body.offsetWidth / 2 - rect.width / 2;
-  let centerY = document.body.offsetHeight / 4 - rect.height / 2;
+  let rect = ball.getBoundingClientRect(),
+    centerX = document.body.offsetWidth / 2 - rect.width / 2,
+    centerY = document.body.offsetHeight / 4 - rect.height / 2;
+
   ball.style.left = centerX + "px";
   ball.style.top = centerY + "px";
-  //ball.positions = [];
-  //  interval = setInterval(updateOrbit, 50);
+
+  interval = setInterval(updateOrbit, 10);
 }
+
 function updateOrbit() {
-  let rect = ball.getBoundingClientRect();
-  let centerX = document.body.offsetWidth / 2 - rect.width / 2;
-  let centerY = document.body.offsetHeight / 2 - rect.height / 2;
-  let hyp = radius;
-  let opp = Math.sin((angle * Math.PI) / 180) * hyp;
-  let adj = Math.cos((angle * Math.PI) / 180) * hyp;
+  let rect = ball.getBoundingClientRect(),
+    centerX = document.body.offsetWidth / 2 - rect.width / 2,
+    centerY = document.body.offsetHeight / 2 - rect.height / 2,
+    hyp = radius,
+    opp = Math.sin((angle * Math.PI) / 180) * hyp,
+    adj = Math.cos((angle * Math.PI) / 180) * hyp;
+
   ball.style.left = centerX + opp + "px";
   ball.style.top = centerY - adj + "px";
-  /*
-  var p1 = {
-    x: centerX,
-    y: centerY
-  };
 
-  var p2 = {
-    x: centerX + opp,
-    y: centerY - adj
-  };
-  //console.log(p1, p2);
-  // angle in degrees
-  var xdist = p2.x - p1.x;
-  var ydist = p2.y - p1.y;
-  var angleDeg = 180 - Math.round((Math.atan2(xdist, ydist) * 180) / Math.PI);
+  ball.style.transform = "rotate(" + angleDeg + "deg)";
 
-  //console.log(angleDeg);
-
-  ball.style.transform = "rotate(" + angleDeg + "deg)";*/
   angle += 2;
   if (angle >= 360) {
     angle - 360;
   }
-  //  ball.style.transform = "rotate(" + angle + "deg)";
 }
+
 ball.onmousedown = function(event) {
   clearInterval(interval);
   let rect = ball.getBoundingClientRect();
+
   ball.shiftX = event.clientX - rect.left;
   ball.shiftY = event.clientY - rect.top;
 
-  //ball.style.position = "absolute";
   ball.style.zIndex = 1000;
-  //document.body.append(ball);
 
-  //moveAt(event.pageX, event.pageY);
-
-  // centers the ball at (pageX, pageY) coordinates
   function moveAt(pageX, pageY) {
-    //console.log("moveAt(" + pageX + "," + pageY + ")");
-
-    let currentX = pageX - ball.shiftX,
-      currentY = pageY - ball.shiftY;
+    let currentX = pageX,
+      currentY = pageY;
 
     ball.style.left = currentX + "px";
     ball.style.top = currentY + "px";
 
     var p1 = {
-      x: document.body.offsetWidth / 2 - rect.width / 2,
-      y: document.body.offsetHeight / 2 - rect.height / 2
+      x: document.body.offsetWidth / 2,
+      y: document.body.offsetHeight / 2
     };
 
     var p2 = {
       x: currentX,
       y: currentY
     };
-    //console.log(p1, p2);
+
     // angle in degrees
-    var xdist = p2.x - p1.x;
-    var ydist = p2.y - p1.y;
-    var angleDeg = 180 - Math.round((Math.atan2(xdist, ydist) * 180) / Math.PI);
+    let xdist = p2.x - p1.x,
+      ydist = p2.y - p1.y,
+      angleDeg = 180 - Math.round((Math.atan2(xdist, ydist) * 180) / Math.PI);
 
     //console.log(angleDeg);
-
-    ball.style.transform = "rotate(" + angleDeg + "deg)";
+    ball.firstElementChild.style.transform = "rotate(" + angleDeg + "deg)";
   }
 
   function onMouseMove(event) {
